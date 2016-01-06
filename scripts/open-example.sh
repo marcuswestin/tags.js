4d1bcb46ac3f91f1155160992794a8a520095e71
Marcus Westin <marcus.westin@gmail.com>
2016-01-06 10:54:34 -0500

    Give tags.js a named budo port

--- scripts/open-example.sh
+++ scripts/open-example.sh
@@ -2,5 +2,5 @@ set -e
 cd $(cd `dirname ${BASH_SOURCE[0]}` && pwd -P)
 export PATH=../node_modules/.bin:$PATH
 
-budo ../examples/exampleReactApp.js --live --open --title "tags react example" -- -d \
+budo ../examples/exampleReactApp.js --port 9901 --live --open --title "tags react example" -- -d \
 	-t babelify --presets es2015
