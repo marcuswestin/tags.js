a6aa9a0f19313b623e553f5034ff0833bd24c75e
Marcus Westin <marcus.westin@gmail.com>
2017-11-01 09:54:02 -0400

    Add helper function List, which takes an optional renderEmptyList function

--- src/tags-helpers.js
+++ src/tags-helpers.js
@@ -11,6 +11,28 @@ export var Map = wrap(function(items, fn) {
 	return _.map(items, fn, this)
 })
 
+// List(items, renderItemFn)
+// List(items, renderEmptyFn, renderItemFn)
+export function List(items, renderEmptyFn, renderItemFn) {
+	if (arguments.length == 2) {
+		// No renderEmptyFn
+		renderItemFn = renderEmptyFn
+		renderEmptyFn = null
+	}
+	if (items != undefined && !_.isArray(items)) {
+		throw new Error('List called with non-array items: '+items)
+	}
+	var isEmpty = (!items || items.length == 0)
+	if (isEmpty && renderEmptyFn) {
+		return renderEmptyFn.call(this)
+	}
+	return _.map(items, (val, i) => {
+		var isFirst = (i == 0)
+		var isLast = (i == items.length - 1)
+		return renderItemFn(val, i, isFirst, isLast)
+	})
+}
+
 export function renderRandomBackgrounds(shouldRender) {
 	renderRandomBackgrounds.shouldRender = shouldRender
 	if (renderRandomBackgrounds.initialized) {
